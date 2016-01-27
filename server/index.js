0b61d0ef739373aefef8397e5a8bec725cd763aa
gios <pblazhchuk@gmail.com>
2016-01-27 17:57:57 +0200

    add sqlite3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-"use strict";
+'use strict';
+const fs = require('fs')
 const app = require('koa')()
 const router = require('koa-router')()
 const serve = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const jwt = require('koa-jwt')
+
+var file = __dirname + '/../base.db'
+var exists = fs.existsSync(file)
+
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database(file)
+
+db.serialize(function() {
+  if (exists) {
+    db.run('CREATE TABLE lorem (info TEXT)')
+  }
+
+  var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+  for (var i = 0; i < 10; i++) {
+    stmt.run('Ipsum ' + i)
+  }
+  stmt.finalize()
+
+  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+    console.log(row.id + ': ' + row.info)
+  })
+})
+
+db.close()
 
 router.get('/api/hello', function *() {
   this.body = 'Hello World!'
