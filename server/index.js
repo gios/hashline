9f0271ef35e0dc8878f6ed3a747994d967ed09a7
gios <pblazhchuk@gmail.com>
2016-01-28 12:08:25 +0200

    db call

--- server/index.js
+++ server/index.js
@@ -13,26 +13,28 @@ var exists = fs.existsSync(file)
 var sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database(file)
 
-db.serialize(function() {
-  if (exists) {
-    db.run('CREATE TABLE lorem (info TEXT)')
-  }
-
-  var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i)
-  }
-  stmt.finalize()
-
-  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-    console.log(row.id + ': ' + row.info)
-  })
-})
-
-db.close()
-
 router.get('/api/hello', function *() {
   this.body = 'Hello World!'
+})
+
+router.get('/api/runSqlite', function *() {
+  db.serialize(function() {
+    if (exists) {
+      db.run('CREATE TABLE lorem (info TEXT)')
+    }
+
+    var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+    for (var i = 0; i < 10; i++) {
+      stmt.run('Ipsum ' + i)
+    }
+    stmt.finalize()
+
+    db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+      console.log(row.id + ': ' + row.info)
+    })
+  })
+
+  db.close()
 })
 
 router.get('/api/fakeData', function *() {
