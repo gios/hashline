827b49865ecf0887ebb590efcaf3be264a4923e7
gios <pblazhchuk@gmail.com>
2016-01-29 18:34:28 +0200

    get user login data

--- server/apis/src/routes/users.js
+++ server/apis/src/routes/users.js
@@ -1,8 +1,11 @@
 module.exports = function(router, db) {
   'use strict';
 
-  router.post('/authenticate', function *(next) {
+  router.get('/api/authenticate', function *(next) {
     db.serialize(function() {
+      db.each('SELECT rowid AS id, name, password FROM Users', function(err, row) {
+        console.log(`${row.id}: User, ${row.name} with password ${row.password}`);
+      });
     })
   })
 }
