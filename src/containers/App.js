4a85116db4affe7dc5e8464e5b43601fe1fe10db
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-02-01 23:54:59 +0200

    add logo simple

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
-import { Link } from 'react-router'
 import { connect } from 'react-redux'
-import { routeActions } from 'react-router-redux'
 import Footer from '../components/parts/Footer'
+import IntroLogo from '../components/parts/IntroLogo'
 
 class App extends Component {
   render() {
-    const { currentRoute } = this.props
     return (
       <div>
-        <nav className='navbar navbar-fixed-top navbar-dark bg-inverse'>
-          <Link className='navbar-brand' to='/'>Sportalking</Link>
-          <ul className='nav navbar-nav'>
-            <li className='nav-item'>
-              <Link className={currentRoute === '/' ? 'nav-link active' : 'nav-link'} to='/'>
-                Home <span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className={currentRoute === '/counter' ? 'nav-link active' : 'nav-link'} to='/counter'>
-                Counter
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className={currentRoute === '/wonders' ? 'nav-link active' : 'nav-link'} to='/wonders'>
-                Wonders
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <IntroLogo/>
         <div>
           {this.props.children}
         </div>
@@ -40,8 +19,7 @@ class App extends Component {
 
 function inject(state) {
   return {
-    push: routeActions.push,
-    currentRoute: state.routing.location.pathname
+    state
   }
 }
 
