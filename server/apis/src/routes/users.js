4e0cbdb4d36d28104f5f2b99ad1ede603ca074e1
gios <pblazhchuk@gmail.com>
2016-02-04 14:49:30 +0200

    add db register and auth

--- server/apis/src/routes/users.js
+++ server/apis/src/routes/users.js
@@ -7,19 +7,16 @@ module.exports = function(router, jwt, SHARED_SECRET) {
   UserMethods.initUsersTable()
 
   router.post('/authenticate', function *(next) {
-    if (!(this.request.body.email === 'john@example.com' && this.request.body.password === 'foo')) {
-      this.status = 401
-      this.body = 'Wrong user or password'
-      return;
-    }
+    let email = this.request.body.email
+    let password = this.request.body.password
 
-    let profile = {
-      username: 'John',
-      email: 'john@example.com',
-      id: 123
-    };
+    let foundUser = yield knex('users').where({
+      email,
+      password
+    })
+    .first('username', 'email', 'id')
 
-    let token = jwt.sign(profile, SHARED_SECRET, { expiresIn: 60 * 5 });
+    let token = jwt.sign(foundUser, SHARED_SECRET, { expiresIn: 60 * 5 });
     this.body = { id_token: token }
     yield next
   })
@@ -40,9 +37,12 @@ module.exports = function(router, jwt, SHARED_SECRET) {
       logger.error(error)
     })
 
-    let user = yield knex('users').where('id', userId[0])
+    let user = yield knex('users')
+    .where('id', userId[0])
+    .first('username', 'email', 'id')
 
-    this.body = user
+    let token = jwt.sign(user, SHARED_SECRET, { expiresIn: 60 * 5 });
+    this.body = { id_token: token }
     yield next
   })
 }
