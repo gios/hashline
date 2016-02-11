290ba227b95805424f1cc1c67054d8fceaca3db4
gios <pblazhchuk@gmail.com>
2016-02-11 17:52:30 +0200

    add default error message

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.use(function *(next) {
   try {
     yield next;
   } catch (err) {
-    this.status = err.status || 500;
-    this.body = err.message;
-    this.app.emit('error', err, this);
+    this.status = err.status || 500
+    this.body = err.message || 'Internal server error'
+    this.app.emit('error', err, this)
   }
 })
 app.use(logger())
