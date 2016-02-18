50d62ef0c7f4e54a18dc9d496c41425177a17aaa
gios <pblazhchuk@gmail.com>
2016-02-18 16:12:31 +0200

    dropdown menu

--- src/components/dash/IndexDash.js
+++ src/components/dash/IndexDash.js
@@ -21,33 +21,86 @@ class IndexDash extends Component {
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-futbol-o'></i>
+              <a className='nav-link sidebar-link' data-toggle='collapse' href='#soccerCollapse' aria-expanded='false' aria-controls='soccerCollapse'>
+                <i className='fa fa-angle-down'></i>
                 <span className='sidebar-list-item'>Soccer</span>
               </a>
             </li>
+            <div className='collapse' id='soccerCollapse'>
+              <ul className='nav sidebar-list-static'>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>English Premier League</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>Bundesliga</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>La Liga</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>Ukrainian Premier League</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>Serie A</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>Eredivisie</span>
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a className='nav-link sidebar-link-nested' href='#'>
+                    <i className='fa fa-angle-right'></i>
+                    <span className='sidebar-list-item'>Major League Soccer</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
             <li className='nav-item'>
               <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-camera-retro'></i>
+                <i className='fa fa-angle-down'></i>
                 <span className='sidebar-list-item'>Basketball</span>
               </a>
             </li>
             <li className='nav-item'>
               <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-camera-retro'></i>
+                <i className='fa fa-angle-down'></i>
                 <span className='sidebar-list-item'>American Football</span>
               </a>
             </li>
             <li className='nav-item'>
               <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-camera-retro'></i>
+                <i className='fa fa-angle-down'></i>
                 <span className='sidebar-list-item'>Hockey</span>
               </a>
             </li>
             <li className='nav-item'>
               <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-camera-retro'></i>
+                <i className='fa fa-angle-down'></i>
                 <span className='sidebar-list-item'>Tennis</span>
+              </a>
+            </li>
+            <li className='nav-item deactivate'></li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-rss'></i>
+                <span className='sidebar-list-item'>Feed</span>
               </a>
             </li>
           </ul>
