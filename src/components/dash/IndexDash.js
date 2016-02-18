91d8d40a7fa308d43a242825a48e08a7f02d9ee5
gios <pblazhchuk@gmail.com>
2016-02-18 17:56:17 +0200

    throttle function

--- src/components/dash/IndexDash.js
+++ src/components/dash/IndexDash.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
 import moment from 'moment'
+import { throttle } from '../../utils/helpers'
 
 class IndexDash extends Component {
+
+  componentDidMount() {
+    window.addEventListener('resize', throttle(() => {
+      console.log(1)
+    }))
+  }
 
   render() {
     return (
@@ -13,108 +20,106 @@ class IndexDash extends Component {
             <div>oneal@gmail.com</div>
           </div>
         </div>
-        <div className='collapse navbar-toggleable-xs'>
-          <ul className='nav sidebar-list-static'>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link active' href='#'>
-                <i className='fa fa-dashcube'></i>
-                <span className='sidebar-list-item'>Dash</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' data-toggle='collapse' href='#soccerCollapse' aria-expanded='false' aria-controls='soccerCollapse'>
-                <i className='fa fa-angle-down'></i>
-                <span className='sidebar-list-item'>Soccer</span>
-              </a>
-            </li>
-            <div className='collapse' id='soccerCollapse'>
-              <ul className='nav sidebar-list-static nested'>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>English Premier League</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>Bundesliga</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>La Liga</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>Ukrainian Premier League</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>Serie A</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>Eredivisie</span>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link sidebar-link nested' href='#'>
-                    <i className='fa fa-chevron-circle-right'></i>
-                    <span className='sidebar-list-item'>Major League Soccer</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-angle-right'></i>
-                <span className='sidebar-list-item'>Basketball</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-angle-right'></i>
-                <span className='sidebar-list-item'>American Football</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-angle-right'></i>
-                <span className='sidebar-list-item'>Hockey</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-angle-right'></i>
-                <span className='sidebar-list-item'>Tennis</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link deactivate'></a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-rss'></i>
-                <span className='sidebar-list-item'>Feed</span>
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link sidebar-link' href='#'>
-                <i className='fa fa-bell-o'></i>
-                <span className='label label-default label-pill pull-xs-right'>14</span>
-                <span className='sidebar-list-item'>Notification</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className='nav sidebar-list-static'>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link active' href='#'>
+              <i className='fa fa-dashcube'></i>
+              <span className='sidebar-list-item'>Dash</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' data-toggle='collapse' href='#soccerCollapse' aria-expanded='false' aria-controls='soccerCollapse'>
+              <i className='fa fa-angle-down'></i>
+              <span className='sidebar-list-item'>Soccer</span>
+            </a>
+          </li>
+          <div className='collapse' id='soccerCollapse'>
+            <ul className='nav sidebar-list-static nested'>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>English Premier League</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>Bundesliga</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>La Liga</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>Ukrainian Premier League</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>Serie A</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>Eredivisie</span>
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link sidebar-link nested' href='#'>
+                  <i className='fa fa-chevron-circle-right'></i>
+                  <span className='sidebar-list-item'>Major League Soccer</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-angle-right'></i>
+              <span className='sidebar-list-item'>Basketball</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-angle-right'></i>
+              <span className='sidebar-list-item'>American Football</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-angle-right'></i>
+              <span className='sidebar-list-item'>Hockey</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-angle-right'></i>
+              <span className='sidebar-list-item'>Tennis</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link deactivate'></a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-rss'></i>
+              <span className='sidebar-list-item'>Feed</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link sidebar-link' href='#'>
+              <i className='fa fa-bell-o'></i>
+              <span className='label label-default label-pill pull-xs-right'>14</span>
+              <span className='sidebar-list-item'>Notification</span>
+            </a>
+          </li>
+        </ul>
         <div className='navbar-footer'>
           <span className='label label-success text-xs-center'>Alpha v0.1.1</span>
           <p>&copy; {moment().format('YYYY')}</p>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,25 @@ export function DOMtoArray(obj) {
   }
   return array;
 }
+export function throttle(fn, threshhold, scope) {
+  threshhold || (threshhold = 250)
+  let last
+  let deferTimer
+
+  return () => {
+    let context = scope || this
+    let now = +new Date
+    let args = arguments
+
+    if (last && now < last + threshhold) {
+      clearTimeout(deferTimer)
+      deferTimer = setTimeout(() => {
+        last = now
+        fn.apply(context, args)
+      }, threshhold)
+    } else {
+      last = now
+      fn.apply(context, args)
+    }
+  }
+}
