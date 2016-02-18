c500c61b26a955df2c1ec693216641451b153e62
gios <pblazhchuk@gmail.com>
2016-02-18 15:02:20 +0200

    dash

--- src/components/dash/IndexDash.js
+++ src/components/dash/IndexDash.js
@@ -5,13 +5,51 @@ class IndexDash extends Component {
   render() {
     return (
       <nav role='navigation' className='navbar navbar-dark navbar-static'>
+        <div className='navbar-info'>
+          <p className='navbar-logo'>Sportalking</p>
+          <div className='navbar-user-info'>
+            <div>Oneal</div>
+            <div>oneal@gmail.com</div>
+          </div>
+        </div>
         <div className='collapse navbar-toggleable-xs'>
-          <ul className='nav'>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+          <ul className='nav sidebar-list-static'>
+            <li className='nav-item active'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-dashcube'></i>
+                <span className='sidebar-list-item'>Dash</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-futbol-o'></i>
+                <span className='sidebar-list-item'>Soccer</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-camera-retro'></i>
+                <span className='sidebar-list-item'>Basketball</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-camera-retro'></i>
+                <span className='sidebar-list-item'>American Football</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-camera-retro'></i>
+                <span className='sidebar-list-item'>Hockey</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link sidebar-link' href='#'>
+                <i className='fa fa-camera-retro'></i>
+                <span className='sidebar-list-item'>Tennis</span>
+              </a>
+            </li>
           </ul>
         </div>
       </nav>
