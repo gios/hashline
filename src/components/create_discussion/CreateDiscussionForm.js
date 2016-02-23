981e6c47f5eef0ee868a065444f4460b0b9dd2bb
gios <pblazhchuk@gmail.com>
2016-02-23 13:59:51 +0200

    progress create discussion

--- src/components/create_discussion/CreateDiscussionForm.js
+++ src/components/create_discussion/CreateDiscussionForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
 import ReactTags from 'react-tag-input'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
 
 class createDiscussionForm extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      tags: [ {id: 1, text: "Apples"} ],
-      suggestions: ["Banana", "Mango", "Pear", "Apricot"]
+      tags: [ {id: 1, text: 'Apples'} ],
+      suggestions: ['Banana', 'Mango', 'Pear', 'Apricot'],
+      startDate: moment()
     }
+  }
+
+  handleChange(date) {
+    this.setState({ startDate: date })
   }
 
   handleDelete(i) {
@@ -40,7 +47,7 @@ class createDiscussionForm extends Component {
   render() {
     return (
       <div className='card-group'>
-        <div className='card col-xs-12 col-sm-12 col-md-12 col-lg-6'>
+        <div className='card col-xs-12 col-sm-12 col-md-12 col-lg-6 create-discussion-block'>
           <div className='card-block'>
             <h4 className='card-title text-xs-center'>Create Discussion</h4>
             <form>
@@ -56,25 +63,54 @@ class createDiscussionForm extends Component {
                   <textarea className='form-control' id='inputDescription' rows='3' placeholder='Describe your conversation'></textarea>
                 </div>
               </fieldset>
+            </form>
+          </div>
+        </div>
+        <div className='card col-xs-12 col-sm-12 col-md-12 col-lg-6 create-discussion-block'>
+          <div className='card-block'>
+            <h4 className='card-title text-xs-center'>Settings</h4>
+            <form>
               <fieldset className='form-group row'>
-                <label htmlFor='inputModerators' className='col-sm-2 form-control-label'>Moderators</label>
+                <div className='checkbox'>
+                  <label>
+                    <input type='checkbox' value=''/>
+                    Private <small className='text-muted'>use this if you want to set password for this conversation</small>
+                  </label>
+                </div>
+              </fieldset>
+              <fieldset className='form-group row'>
+                <div className='col-sm-10'>
+                  <div className='input-group'>
+                    <div className='input-group-addon'>Password</div>
+                    <input type='password' className='form-control'/>
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset className='form-group row'>
+                <div className='checkbox'>
+                  <label>
+                    <input type='checkbox' value=''/>
+                    Limited <small className='text-muted'>set life cycle for this conversation</small>
+                  </label>
+                </div>
+              </fieldset>
+              <fieldset className='form-group row'>
+                <div className='col-sm-10'>
+                  <DatePicker selected={this.state.startDate} onChange={this.handleChange.bind(this)}/>
+                </div>
+              </fieldset>
+              <fieldset className='form-group row'>
+                <label htmlFor='inputModerators' className='col-sm-2 form-control-label'>Tags</label>
                 <div className='col-sm-12'>
                   <ReactTags.WithContext tags={this.state.tags}
                                          suggestions={this.state.suggestions}
-                                         placeholder='Add moderators'
+                                         placeholder='Add tags'
                                          handleDelete={this.handleDelete.bind(this)}
                                          handleAddition={this.handleAddition.bind(this)}
                                          handleDrag={this.handleDrag.bind(this)}/>
                 </div>
               </fieldset>
             </form>
-          </div>
-        </div>
-        <div className='card col-xs-12 col-sm-12 col-md-12 col-lg-6'>
-          <div className='card-block'>
-            <h4 className='card-title text-xs-center'>Settings</h4>
-            <p className='card-text'>This card has supporting text below as a natural lead-in to additional content.</p>
-            <p className='card-text'><small className='text-muted'>Last updated 3 mins ago</small></p>
           </div>
         </div>
       </div>
