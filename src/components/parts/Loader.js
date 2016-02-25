0a8d795877ee185daea4b625a5b9f09d2ad15b02
gios <pblazhchuk@gmail.com>
2016-02-25 18:19:58 +0200

    loader component

---
+++ src/components/parts/Loader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Loader extends Component {
+
+  render() {
+    return (
+      <div className='loader'>Loading...</div>
+    )
+  }
+}
+
+export default Loader
