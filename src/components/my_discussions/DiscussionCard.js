45b27561f1fde2102a469f31ba7c42785a92cffc
gios <pblazhchuk@gmail.com>
2016-03-09 14:05:57 +0200

    join button

--- src/components/my_discussions/DiscussionCard.js
+++ src/components/my_discussions/DiscussionCard.js
@@ -8,9 +8,10 @@ class DiscussionCard extends Component {
 
     return (
         <div className='card card-block col-xs-12 col-sm-12 col-md-12 col-lg-6 my-discussion-card'>
+          <button type='button' className='btn btn-success discussion-join-btn m-x-1'>Join</button>
           <h4 className='card-title'>{name}</h4>
           <div className='my-discussion-labels'>
-            {(isPrivate) ? <span className='label label-warning'>Private</span> : <span className='label label-success'>Public</span>}
+            {(isPrivate) ? <span className='label label-warning'>Private</span> : <span className='label label-primary'>Public</span>}
             {(isLimited) ? <span className='label label-info'>Limited</span> : null}
           </div>
           <p className='card-text'><small className='text-muted'>Description: </small>{description}</p>
