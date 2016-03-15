26ab508bbfb10f651d34f72e99f42a9b362d10f1
gios <pblazhchuk@gmail.com>
2016-03-15 11:40:55 +0200

    no discussion message

--- src/components/my_discussions/MyDiscussionsBlock.js
+++ src/components/my_discussions/MyDiscussionsBlock.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Loader from '../parts/Loader'
+import NoDiscussionCards from './NoDiscussionCards'
+import { Link } from 'react-router'
 import DiscussionCard from './DiscussionCard'
 
 class MyDiscussionsBlock extends Component {
@@ -14,6 +16,17 @@ class MyDiscussionsBlock extends Component {
     if(discussions.isFetching) {
       return <Loader size={4} color='red'/>
     } else if(discussions.payload) {
+      if(!discussions.payload.length) {
+        return (
+          <NoDiscussionCards>
+            <h3>You don't have any discussions.</h3><br/>
+            <p>Do you wanna create a discussion?</p>
+            <Link to='/create' type='button' className='btn btn-success btn-sm' role='button'>
+              Create Discussion
+            </Link>
+          </NoDiscussionCards>
+        )
+      }
       return discussions.payload.map((discussion) => {
         return (
           <DiscussionCard onJoinDiscussion={onJoinDiscussion} key={discussion.id} discussion={discussion}/>
