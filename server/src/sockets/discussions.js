ffa8e395e6910451cd305f3e820f503250d5f7fd
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-05-22 18:57:47 +0300

    add migration

--- server/src/sockets/discussions.js
+++ server/src/sockets/discussions.js
@@ -4,11 +4,18 @@ module.exports = function(io, socket) {
   const knex = require('../knexConfig')
 
   socket.on('user-connected', (params) => {
-    knex('users').select('username', 'email')
+    knex('users').select('id', 'username', 'email')
     .where('email', params.userEmail)
     .first()
     .then((user) => {
-      io.emit('user-connected', user)
+      knex('connected_discussion_users')
+      .insert({
+        discussion_id: params.discussionId,
+        user_id: user.id
+      })
+      .then(() => {
+        io.emit('user-connected', { username: user.username, email: user.email })
+      })
     })
   })
 }
