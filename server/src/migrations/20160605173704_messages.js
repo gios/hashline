3a6ffc34f32873bd11b9bcd5abde2699df839c29
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-06-05 17:46:05 +0300

    message migration

---
+++ server/src/migrations/20160605173704_messages.js
@@ -0,0 +1,25 @@
+const logger = require('tracer').colorConsole()
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.hasTable('messages').then((exists) => {
+      if(!exists) {
+        return knex.schema.createTable('messages', (table) => {
+          table.increments()
+          table.integer('discussion_id').unsigned().references('discussions.id')
+          table.integer('user_id').unsigned().references('users.id')
+          table.string('message')
+          table.timestamp('created_at').defaultTo(knex.fn.now())
+          table.timestamp('updated_at').defaultTo(knex.fn.now())
+        })
+        .then(() => logger.info('MESSAGES table has been created'))
+      }
+    })
+  ])
+}
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('messages')
+  ])
+}
