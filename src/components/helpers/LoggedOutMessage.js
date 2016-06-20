a5014b5281820a5870238dbf74ada90157ed6e51
gios <pblazhchuk@gmail.com>
2016-06-20 17:34:53 +0300

    logout message

--- src/components/helpers/LoggedOutMessage.js
+++ src/components/helpers/LoggedOutMessage.js
@@ -7,7 +7,7 @@ class LoggedOutMessage extends Component {
       <div className='logged-out-message'>
         You have been logged out of the Hashline
         <button type='button'
-                className='btn btn-warning btn-sm pull-xs-right'
+                className='btn btn-warning btn-sm logged-out-button'
                 onClick={this.props.onLogout.bind(this)}>Logout</button>
       </div>
     )
