186dbea709b123f6a9eed9473ce2b6b28df8803f
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-06-20 20:15:44 +0300

    bump version

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const VERSION = 'Alpha v0.3.19'
+export const VERSION = 'Alpha v0.3.21'
 export const MOBILE_MAX_WIDTH = 1376
 export const ENTER_KEYCODE = 13
 export const MESSAGE_INTERVAL = 100
