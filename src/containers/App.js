63ba3c2a1a88e7a509911eec045a2d310e06fe27
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-06-24 19:35:59 +0300

    refresh event

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,6 +28,7 @@ class App extends Component {
 
     socket.on('invite users', notificationsData => {
       dispatch(setSentNotificationsArchive(notificationsData))
+      dispatch(getUserData())
     })
   }
 
