dba5289aac77926779b4a42aeeb1e5f5e77bf640
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-06-26 13:29:42 +0300

    remove comment

--- server/src/sockets/discussions.js
+++ server/src/sockets/discussions.js
@@ -84,17 +84,6 @@ module.exports = function(io, socket) {
 
     socket.join(`discussion-${params.discussionId}`)
     socket.broadcast.to(`discussion-${params.discussionId}`).emit('join discussion', params.username)
-
-    // knex('discussions')
-    // .select('is_private')
-    // .where('id', params.discussionId)
-    // .then(is_private => {
-    //   console.log(is_private.length)
-    //   if(!is_private.length) {
-    //     socket.join(`discussion-${params.discussionId}`)
-    //     socket.broadcast.to(`discussion-${params.discussionId}`).emit('join discussion', params.username)
-    //   }
-    // })
   })
 
   socket.on('leave discussion', params => {
