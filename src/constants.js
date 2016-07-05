959d958cc49627d4921e3372d76cfa27f3c5ad0f
gios <pblazhchuk@gmail.com>
2016-07-05 11:44:09 +0300

    bump version

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const VERSION = 'Beta v0.5.2'
+export const VERSION = 'Beta v0.5.3'
 export const MOBILE_MAX_WIDTH = 1376
 export const ENTER_KEYCODE = 13
 export const MESSAGE_INTERVAL = 100
