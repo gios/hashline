ed0cbf5d77c95f088323cf5276f049ca7c3ab70f
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-07-07 22:14:02 +0300

    hotfix

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,12 @@ module.exports = [
   /^\/authenticate/,   // *** /authenticate
   /^\/registration/,   // *** /registration
   /^\/create/,         // *** /create
+  /^\/type\/\d*/,      // *** /type:name
+  /^\/search/,         // *** /search
+  /^\/trending/,       // *** /trending
+  /^\/recent/,         // *** /recent
+  /^\/mostdiscussed/,  // *** /mostdiscussed
+  /^\/limited/,        // *** /limited
   /^\/mydiscussions/,  // *** /mydiscussions
   /^\/discussion\/\d*/ // *** /discussion:id
 ]
