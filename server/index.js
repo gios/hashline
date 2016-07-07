d03ffd8a498132b8af7dc7274ec98aecc1cd47f3
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-07-07 21:36:16 +0300

    whitespace

--- server/index.js
+++ server/index.js
@@ -17,43 +17,43 @@ const tracer = require('tracer').colorConsole()
 
 const SHARED_SECRET = 'hashline'
 
-  app.use(function *(next) {
-    yield send(this, path.resolve(__dirname, '/../public/', 'index.html'))
+app.use(function *(next) {
+  yield send(this, path.resolve(__dirname, '/../public/', 'index.html'))
+  yield next
+})
+
+app.use(function *(next) {
+  try {
     yield next
-  })
-
-  app.use(function *(next) {
-    try {
-      yield next
-    } catch (err) {
-      if (400 < this.status && this.status < 500) {
-        tracer.warn(err)
-      } else {
-        tracer.error(err)
-      }
-
-      this.status = err.status || 500
-      this.body = { code: this.status, message: err.message || 'Internal server error' }
-      this.app.emit('error', err, this)
+  } catch (err) {
+    if (400 < this.status && this.status < 500) {
+      tracer.warn(err)
+    } else {
+      tracer.error(err)
     }
-  })
 
-  app.use(logger())
-  app.use(helmet())
-  app.use(bodyParser())
-  app.use(serve(__dirname + '/../public'))
-  app.use(favicon(__dirname + '/../public/favicon.ico'))
-  app.use(jwt({ secret: SHARED_SECRET }).unless({ path: routes }))
-  app.use(router.routes())
-  app.use(router.allowedMethods())
+    this.status = err.status || 500
+    this.body = { code: this.status, message: err.message || 'Internal server error' }
+    this.app.emit('error', err, this)
+  }
+})
 
-  require('./src/routes/userRoutes')(router, jwt, SHARED_SECRET)
-  require('./src/routes/discussionRoutes')(router)
+app.use(logger())
+app.use(helmet())
+app.use(bodyParser())
+app.use(serve(__dirname + '/../public'))
+app.use(favicon(__dirname + '/../public/favicon.ico'))
+app.use(jwt({ secret: SHARED_SECRET }).unless({ path: routes }))
+app.use(router.routes())
+app.use(router.allowedMethods())
 
-  io.on('connection', (socket) => {
-    tracer.info('USER CONNECTED')
-    require('./src/sockets/discussions')(io, socket)
-  })
+require('./src/routes/userRoutes')(router, jwt, SHARED_SECRET)
+require('./src/routes/discussionRoutes')(router)
 
-  server.listen(process.env.PORT || 5000)
-  tracer.info('Hashline is running on port', process.env.PORT || 5000)
+io.on('connection', (socket) => {
+  tracer.info('USER CONNECTED')
+  require('./src/sockets/discussions')(io, socket)
+})
+
+server.listen(process.env.PORT || 5000)
+tracer.info('Hashline is running on port', process.env.PORT || 5000)
