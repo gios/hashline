83c60ac0878f4156f5b73ec635f173f1d9e2d528
gios <pblazhchuk@gmail.com>
2016-07-08 15:40:22 +0300

    rank table

--- src/components/dash/UserStatusInfo.js
+++ src/components/dash/UserStatusInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { DropModal } from 'boron'
 import Loader from '../parts/Loader'
 
 class UserSTatusInfo extends Component {
@@ -23,6 +24,56 @@ class UserSTatusInfo extends Component {
     return <span className={`label ${rankColor}`}>{rank}</span>
   }
 
+  getRankTableContent() {
+    return (
+      <div className='text-xs-center'>
+        <div className='modal-header'>
+          <h4 className='modal-title'>Rank Table</h4>
+        </div>
+        <div className='modal-body'>
+          <div className='table-responsive'>
+            <table className="table">
+              <thead className="thead-inverse">
+                <tr>
+                  <th>Rank</th>
+                  <th>Name</th>
+                  <th>Discussions created</th>
+                  <th>Messages Sent</th>
+                  <th>Messages Received</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <form onSubmit={() => this.refs.deleteModal.toggle()}>
+          <div className='modal-footer'>
+            <button type='submit' className='btn btn-primary'>Back</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
   renderUserStatusInfo() {
     let { dashUserInfo } = this.props
 
@@ -31,7 +82,9 @@ class UserSTatusInfo extends Component {
     } else if(dashUserInfo.payload) {
       return (
         <div className='card'>
+          <DropModal ref='rankTable' modalStyle={{width: '50%'}}>{this.getRankTableContent()}</DropModal>
           <div className='card-block'>
+            <button onClick={() => this.refs.rankTable.toggle()} type='button' className='btn btn-sm btn-primary pull-xs-right'>Rank Table</button>
             <h4 className='card-title'>Rank {this.rankLabelSelector(dashUserInfo.payload.rank)}</h4>
             <p className='card-text'>This rank is calculated using the ratio between the created and received messages from your discussions.</p>
           </div>
