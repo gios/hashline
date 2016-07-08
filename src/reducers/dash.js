9c6403b1601e150f15ba76f0e7dd7c5a0363c0f0
gios <pblazhchuk@gmail.com>
2016-07-08 16:05:59 +0300

    remove dublicate code

--- src/reducers/dash.js
+++ src/reducers/dash.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
-import Immutable from 'immutable'
 import { apiRequestInit, apiRequestInitState } from './helpers'
 import { REQUEST_GET_MY_TRENDING_DISCUSSIONS,
          SUCCESS_GET_MY_TRENDING_DISCUSSIONS,
@@ -11,70 +10,16 @@ import { REQUEST_GET_MY_TRENDING_DISCUSSIONS,
          SUCCESS_DASH_USERS_RANK,
          FAILURE_DASH_USERS_RANK } from '../actions/dashAction'
 
-const getMyTrendingDiscussionsState = Immutable.Map({
-  isFetching: false,
-  payload: null,
-  error: false
-})
-
-function myTrendingDiscussions(state = getMyTrendingDiscussionsState, action) {
-  switch (action.type) {
-    case REQUEST_GET_MY_TRENDING_DISCUSSIONS:
-      return state.merge({
-        isFetching: true,
-        payload: null,
-        error: false
-      })
-    case SUCCESS_GET_MY_TRENDING_DISCUSSIONS:
-      return state.merge({
-        isFetching: false,
-        payload: action.payload,
-        error: false
-      })
-    case FAILURE_GET_MY_TRENDING_DISCUSSIONS:
-      return state.merge({
-        isFetching: false,
-        payload: action.payload.response,
-        error: true
-      })
-    default:
-      return state
-  }
-}
-
-const getDashUserInfoState = Immutable.Map({
-  isFetching: false,
-  payload: null,
-  error: false
-})
-
-function dashUserInfo(state = getDashUserInfoState, action) {
-  switch (action.type) {
-    case REQUEST_DASH_USER_INFO:
-      return state.merge({
-        isFetching: true,
-        payload: null,
-        error: false
-      })
-    case SUCCESS_DASH_USER_INFO:
-      return state.merge({
-        isFetching: false,
-        payload: action.payload,
-        error: false
-      })
-    case FAILURE_DASH_USER_INFO:
-      return state.merge({
-        isFetching: false,
-        payload: action.payload.response,
-        error: true
-      })
-    default:
-      return state
-  }
-}
-
 function dashUsersRank(state = apiRequestInitState, action) {
   return apiRequestInit(state, action, REQUEST_DASH_USERS_RANK, SUCCESS_DASH_USERS_RANK, FAILURE_DASH_USERS_RANK)
+}
+
+function dashUserInfo(state = apiRequestInitState, action) {
+  return apiRequestInit(state, action, REQUEST_DASH_USER_INFO, SUCCESS_DASH_USER_INFO, FAILURE_DASH_USER_INFO)
+}
+
+function myTrendingDiscussions(state = apiRequestInitState, action) {
+  return apiRequestInit(state, action, REQUEST_GET_MY_TRENDING_DISCUSSIONS, SUCCESS_GET_MY_TRENDING_DISCUSSIONS, FAILURE_GET_MY_TRENDING_DISCUSSIONS)
 }
 
 export let dash = combineReducers({
