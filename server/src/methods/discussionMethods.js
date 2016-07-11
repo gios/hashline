408980097a40748c970fce6190e44969d6d2b9e8
gios <pblazhchuk@gmail.com>
2016-07-11 18:34:09 +0300

    error handling

--- server/src/methods/discussionMethods.js
+++ server/src/methods/discussionMethods.js
@@ -1,15 +1,28 @@
 'use strict'
 
 const crypto = require('crypto')
+const tracer = require('tracer').colorConsole()
 
 exports.cryptoPassword = function(password) {
   const cipher = crypto.createCipher('aes192', 'salt')
   cipher.update(password)
+  try {
+    cipher.final('hex')
+  } catch (error) {
+    tracer.error(error)
+    return
+  }
   return cipher.final('hex')
 }
 
 exports.encryptoPassword = function(hash) {
   const decipher = crypto.createDecipher('aes192', 'salt')
   decipher.update(hash, 'hex')
+  try {
+    decipher.final('utf8')
+  } catch (error) {
+    tracer.error(error)
+    return
+  }
   return decipher.final('utf8')
 }
