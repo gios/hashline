1e1e447e35e3510cb8c54af99474085eb0e5656f
Pavlo Blazhchuk <gios@users.noreply.github.com>
2016-07-28 21:08:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-// React, Redux
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 
-// Routing
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-
-// Containers
 import Containers from './containers/Index'
-
-// Store
 import configureStore from './store/configureStore'
-
-// Styles (SCSS)
 import './index.scss'
 
-// Bootstrap, jQuery, Tether
 import $ from 'jquery'
 import Tether from 'tether'
 window.$ = window.jQuery = $
